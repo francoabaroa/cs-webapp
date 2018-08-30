3348b490b9929dbf7f25149f758740c01ce5b73d
Franco Abaroa <franco9395@gmail.com>
2018-08-30 01:05:52 -0500

    minor style changes

--- src/components/Walkthrough.js
+++ src/components/Walkthrough.js
@@ -465,6 +465,22 @@ class Walkthrough extends Component {
     );
   }
 
+  renderScene17(props) {
+    const { classes } = props;
+    return (
+      <div>
+        <Paper className={classes.paper} elevation={1}>
+          <Typography variant="headline" component="h3">
+            This is a sheet of paper.
+          </Typography>
+          <Typography component="p">
+            Paper can be used to build surface or other elements for your application.
+          </Typography>
+        </Paper>
+      </div>
+    );
+  }
+
   render() {
     const { classes } = this.props;
     const { spacing, currentSceneNumber } = this.state;
