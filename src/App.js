da01a1fc2263dd393c216781821bf6a84cab35f2
Franco Abaroa <franco9395@gmail.com>
2018-09-18 09:21:30 -0700

    enable survey

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     super(props);
     this.state = {
       activeStep: 0,
-      enableWalkthrough: false,
+      enableWalkthrough: true,
       checkedExchanges: [],
       cryptoRiskProfile: '',
       cryptoCurrentStatus: '',
